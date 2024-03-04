c99a12f27f107d8319d7bd6b1c2ef8a8708190b7
zSantiagoS <144070550+zSantiagoS@users.noreply.github.com>
2024-03-03 19:18:37 -0500

    Main.java

--- taller programacion/taller1/codigo2/Main.java
+++ taller programacion/taller1/codigo2/Main.java
@@ -3,69 +3,93 @@ public class Main {
         // Creación e inicialización de instancias para realizar pruebas
 
         // Creación de un estudiante
-        Estudiante estudiante = new Estudiante("Juan", "Pérez", "123456789", 20);
-        System.out.println("Estudiante creado: " + estudiante.obtenerInformacion());
+        System.out.println("\n--------------------[ESTUDIANTE]--------------------\n");
+        Estudiante estudiante = new Estudiante("Fede", "Rico", "561651651", 69);
+        System.out.println("Estudiante creado: " + estudiante.obtenerInformacion() + "\n");
+
+        Estudiante estudiante1 = new Estudiante("Santiago", "Sabogal", "7852222221", 19);
+        System.out.println("Estudiante creado: " + estudiante1.obtenerInformacion() + "\n");
 
         //Inscripción en una materia
+        System.out.println("--------------------[AÑADIR MATERIAS]--------------------\n");
+
         Materia matematicas = new Materia("Matemáticas", "MAT101", 30);
         estudiante.inscribirMateria(matematicas);
         matematicas.agregarEstudiante(estudiante);
-        System.out.println("Estudiante inscrito en Matemáticas");
+        System.out.println("Estudiante inscrito en Matemáticas ");
 
-        Materia sexanal = new Materia("Sexanal", "SEX102", 30);
-        estudiante.inscribirMateria(sexanal);
-        sexanal.agregarEstudiante(estudiante);
+        Materia fisica = new Materia("Fisica", "FIS102", 30);
+        estudiante.inscribirMateria(fisica);
+        estudiante1.inscribirMateria(fisica);
+        fisica.agregarEstudiante(estudiante);
+        fisica.agregarEstudiante(estudiante1);
 
-        System.out.println("Estudiante inscrito en Sexanal");
+        System.out.println("\nEstudiante inscrito en Fisica \n");
 
         //  Registro de calificaciones
+        System.out.println("--------------------[REGISTRO DE CALIFICACIONES]--------------------\n");
         estudiante.registrarCalificacion(matematicas, 4, 3, 5, 4);
-        System.out.println("Calificaciones registradas para Matemáticas"); 
+        System.out.println("Calificaciones registradas para Matemáticas \n"); 
 
-        estudiante.registrarCalificacion(sexanal, 2, 3, 2,1.5 );
-        System.out.println("Calificaciones registradas para Sexanal"); 
+        estudiante.registrarCalificacion(fisica, 2, 3, 2,1.5 );
+        System.out.println("Calificaciones registradas para Fisica"); 
+
+        estudiante1.registrarCalificacion(fisica, 4, 5, 4,4 );
+        System.out.println("Calificaciones registradas para Fisica"); 
         
         // Mostrar calificaciones del estudiante
+        System.out.println("\n--------------------[INFORMACIÓN DEL ESTUDIANTE]--------------------\n");
+
         VistaEstudiante vistaEstudiante = new VistaEstudiante(estudiante);
+        VistaEstudiante vistaEstudiante1 = new VistaEstudiante(estudiante1);
         vistaEstudiante.mostrarMateriasYCalificaciones();
+        vistaEstudiante1.mostrarMateriasYCalificaciones();
 
         // Pruebas adicionales
-        System.out.println("\n--- Otras pruebas ---");
+        System.out.println("\n--------------------[OTRAS PRUEBAS]--------------------\n");
+
 
         // Creación e inicialización de un panel administrativo
         PanelAdministrativo panelAdministrativo = new PanelAdministrativo();
 
+        // Cierre de una materia
+        System.out.println("--------------------[EJEMPLO CIERRE DE MATERIA]--------------------\n");
+        panelAdministrativo.cerrarMateria(matematicas);
+        System.out.println("Matemáticas cerrada\n");
+
+        panelAdministrativo.cerrarMateria(fisica);
+        System.out.println("Fisica cerrada\n");
+
         // Mostrar si Habilita o no
+        System.out.println("--------------------[HABILITACION]--------------------\n");
+
         panelAdministrativo.registrarNotaHabilitacion(matematicas, estudiante); 
 
-        panelAdministrativo.registrarNotaHabilitacion(sexanal, estudiante); 
+        panelAdministrativo.registrarNotaHabilitacion(fisica, estudiante); 
+
+        panelAdministrativo.registrarNotaHabilitacion(fisica, estudiante1); 
 
         // Asignación de calificación de habilitación
-        panelAdministrativo.asignarNotaDeHabilitacion(estudiante, sexanal, 3, 3 , 3 ,3);
-        System.out.println("Nota de habilitación asignada");
+        panelAdministrativo.asignarNotaDeHabilitacion(estudiante, fisica, 3, 3 , 3 ,3);
+        System.out.println("Nota de habilitación asignada\n");
         
         // Mostrar calificaciones actualizadas del estudiante
+        System.out.println("--------------------[CALIFICACIONES ACTUALIZADAS DEL HABILITANTE]--------------------\n");
         vistaEstudiante.mostrarMateriasYCalificaciones(); 
 
+
         // Asignación de docente a una materia
-        Docente docente = new Docente("Pedro", "García", "987654321", "Doctorado en Artes Sexuales");
+        System.out.println("--------------------[DOCENTE]--------------------\n");
+
+        Docente docente = new Docente("Patricio", "Montez", "61651651", "Doctorado en Analisis Numerico");
         panelAdministrativo.asignarDocenteAMateria(docente, matematicas);
-        System.out.println("Docente asignado a Matemáticas");
-
         System.out.println("Docente creado: " + docente.obtenerInformacion());
+        System.out.println("Docente asignado a Matemáticas \n");
 
-
-        // Cierre de una materia
-        panelAdministrativo.cerrarMateria(matematicas);
-        System.out.println("Matemáticas cerrada");
-
-        panelAdministrativo.cerrarMateria(sexanal);
-        System.out.println("Sexanal cerrada");
 
         // Mostrar lista de estudiantes inscritos en una materia cerrada
+        System.out.println("\n--------------------[MOSTRAR LISTA DE ESTUDIANTES DE LA MATERIA]--------------------\n");
         panelAdministrativo.mostrarEstudiantesPorMateria(matematicas);
-        panelAdministrativo.mostrarEstudiantesPorMateria(sexanal);
+        panelAdministrativo.mostrarEstudiantesPorMateria(fisica);
     }
 }
-
-
